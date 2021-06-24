867ae0079a4ef2d59458462920201693c5a4f429
Nabil Abdellaoui <nabil.abdellaoui@tala.co>
2021-06-24 08:51:00 -0700

    genesis config

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -10,8 +10,6 @@ use sc_service::ChainType;
 use hex_literal::hex;
 
 
-// The URL for the telemetry server.
-// const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
 
 /// Specialized `ChainSpec`. This is a specialization of the general Substrate ChainSpec type.
 pub type ChainSpec = sc_service::GenericChainSpec<GenesisConfig>;
@@ -54,11 +52,11 @@ pub fn get_account_id_from_address<TPublic: Public>(address: &[u8; 32]) -> Accou
 	AccountPublic::from(get_from_address::<TPublic>(address)).into_account()
 }
 
-pub fn authority_keys_from_address(s: &[u8; 32]) -> (AuraId, GrandpaId) {
+pub fn authority_keys_from_address(addr_sr25519: &[u8; 32], addr_ed25519: &[u8; 32]) -> (AuraId, GrandpaId) {
 
 	(
-		get_from_address::<AuraId>(s),
-		get_from_address::<GrandpaId>(s),
+		get_from_address::<AuraId>(addr_sr25519),
+		get_from_address::<GrandpaId>(addr_ed25519),
 	)
 }
 
@@ -67,18 +65,18 @@ pub fn authority_keys_from_address(s: &[u8; 32]) -> (AuraId, GrandpaId) {
 
 // Convert list of addresses to AccountID or authorities
 
-fn list_to_accounts(list: &Vec<[u8; 32]>) -> Vec<AccountId> {
+fn list_to_accounts(list: &Vec<([u8; 32],[u8; 32])>) -> Vec<AccountId> {
 	let mut ans: Vec<AccountId> = vec![] ;
-	for addr in list {
+	for (addr, _) in list {
 		ans.push(get_account_id_from_address::<sr25519::Public>(&addr)) ;
 	}
 	ans
 }
 
-fn list_to_authorities(list: &Vec<[u8; 32]>) -> Vec<(AuraId, GrandpaId)> {
+fn list_to_authorities(list: &Vec<([u8; 32],[u8; 32])>) -> Vec<(AuraId, GrandpaId)> {
 	let mut ans: Vec<(AuraId, GrandpaId)> = vec![] ;
-	for addr in list {
-		ans.push(authority_keys_from_address(&addr)) ;
+	for (addr_sr25519,addr_ed25519) in list {
+		ans.push(authority_keys_from_address(&addr_sr25519, &addr_ed25519)) ;
 	}
 	ans
 }
@@ -91,25 +89,35 @@ const BALANCE_UNIT: u128 = 1000000000000 ;
 
 const INITIAL_AUTHORITIES_BALANCE: u128 = 1000 * BALANCE_UNIT;
 
-fn initial_test_nodes() -> Vec<[u8; 32]> {
+fn initial_test_nodes() -> Vec<([u8; 32],[u8; 32])> {
 	vec![
-		hex!("a4f40f452d0de9f15f5402d14cd6379865162ec2263bd630284cf02853d9263c"), //node1
-		hex!("a273fb4a9e52dc63c64589bdc9455147581ef0fd1b6420c8bd6f50c22a04a805"), //node2
-		hex!("d01e386e8850ac34571f051361c98fb46ead33223bc3d3efa34b453ee987fb38"), //node3
-		hex!("3e96986e5a5bc0b64108f9e3a7ea850ff37debebf053ecb9eb58d8e3c14d8307"), //node4
-		hex!("52f5dbe7eb64d897f038ef3e9e3f49f944366e054b0e23902ead5d71a7147844"), //node5
-		hex!("9ebf4c489c5de1132f90ddd4d1fada40dbea1e7f99b9e30f0cf300728dbd6010")  //sudo
+		// node1
+		(
+			hex!("3277d22306435a4800ebc611216d301cb79d2166b7519a6bdf58b0b6fb523267"),
+			hex!("2bdb6dab53650339faa80a06fde1b27ea46cee14ee0523bfc43d6c37d64f933d")
+		) ,
+
+		// node2
+		(
+			hex!("d021327f24e3ee188449b9264cb401f94caea467656a505fae1fdb0f6eda523b"),
+			hex!("ced4ef1df5f05aabc0ae1f7d5ee499edf765291aa4f5efc0f0b074e059e0ef52")
+		)
 	]
 }
 
-fn initial_main_nodes() -> Vec<[u8; 32]> {
+fn initial_main_nodes() -> Vec<([u8; 32],[u8; 32])> {
 	vec![
-		hex!("a4f808550b9431df7a27c76e9d736a876f28b029a4b5da497643268f058d0578"), //node1
-		hex!("989c9912e45b1c95ddb4145b1fb11285f2679e36a7433d10d03ad629280c5c43"), //node2
-		hex!("6ed8182452a29387f0b3a99cf7a5146522f190637400a97429a55158193f603b"), //node3
-		hex!("369f8a73a279bd4a840175c31d9c03976389da3d34ac6ab87bdb205c6b156d5f"), //node4
-		hex!("0e99f4d8406a1c40a6843e4dd130bb700717e905e88252ef9f2ababf111a6266"), //node5
-		hex!("a47f12db28111cfc0052494f434f4bf2cd316dcd13852d6f64e538d861c23534")  //sudo
+		// node1
+		(
+			hex!("3277d22306435a4800ebc611216d301cb79d2166b7519a6bdf58b0b6fb523267"),
+			hex!("2bdb6dab53650339faa80a06fde1b27ea46cee14ee0523bfc43d6c37d64f933d")
+		) ,
+
+		// node2
+		(
+			hex!("d021327f24e3ee188449b9264cb401f94caea467656a505fae1fdb0f6eda523b"),
+			hex!("ced4ef1df5f05aabc0ae1f7d5ee499edf765291aa4f5efc0f0b074e059e0ef52")
+		)
 	]
 }
 
@@ -122,21 +130,16 @@ pub fn dev_config() -> Result<ChainSpec, String> {
 	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
 
 	Ok(ChainSpec::from_genesis(
-		// Name
 		"Wika Development Environment",
-		// ID
 		"dev",
 		ChainType::Development,
 		move || wika_genesis(
 			wasm_binary,
-			// Initial PoA authorities
 			vec![
 				authority_keys_from_seed("Alice"),
 				authority_keys_from_seed("Bob")
 			],
-			// Sudo account
 			get_account_id_from_seed::<sr25519::Public>("Alice"),
-			// Pre-funded accounts
 			vec![
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
 				get_account_id_from_seed::<sr25519::Public>("Bob"),
@@ -167,11 +170,11 @@ pub fn test_config() -> Result<ChainSpec, String> {
 		"Wika TestNet",
 		// ID
 		"test",
-		ChainType::Development,
+		ChainType::Live,
 		move || wika_genesis(
 			wasm_binary,
 			list_to_authorities(&test_nodes),
-			get_account_id_from_address::<sr25519::Public>(&hex!("9ebf4c489c5de1132f90ddd4d1fada40dbea1e7f99b9e30f0cf300728dbd6010")),
+			get_account_id_from_address::<sr25519::Public>(&hex!("56442d2ddbace4927f284c799c0e706ce7e0df06f395e2c1bbfb967ece5cf053")),
 			list_to_accounts(&test_nodes),
 			true,
 		),
@@ -193,11 +196,9 @@ pub fn main_config() -> Result<ChainSpec, String> {
 	let main_nodes = initial_main_nodes() ;
 
 	Ok(ChainSpec::from_genesis(
-		// Name
 		"Wika MainNet",
-		// ID
 		"main",
-		ChainType::Local,
+		ChainType::Live,
 		move || wika_genesis(
 			wasm_binary,
 			list_to_authorities(&main_nodes),

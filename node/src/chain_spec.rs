c961d81538ca5f0cf3ffc695982db04828e05522
Nabil Abdellaoui <nabil.abdellaoui@tala.co>
2021-06-25 02:38:52 -0700

    config

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,10 +1,10 @@
 use sp_core::{Pair, Public, sr25519};
 use wika_runtime::{
-	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
-	SudoConfig, SystemConfig, AuthoritiesConfig, WASM_BINARY, Signature
+	AccountId, BalancesConfig, GenesisConfig,
+	SudoConfig, SystemConfig, AuraConfig, GrandpaConfig, AuthoritiesConfig, WASM_BINARY, Signature
 };
-use sp_consensus_aura::sr25519::AuthorityId as AuraId;
-use sp_finality_grandpa::AuthorityId as GrandpaId;
+//use sp_consensus_aura::sr25519::AuthorityId as AuraId;
+//use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{Verify, IdentifyAccount};
 use sc_service::ChainType;
 use hex_literal::hex;
@@ -32,12 +32,12 @@ pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
 	AccountPublic::from(get_from_seed::<TPublic>(seed)).into_account()
 }
 
-pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
+/*pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
 	(
 		get_from_seed::<AuraId>(s),
 		get_from_seed::<GrandpaId>(s),
 	)
-}
+}*/
 
 
 // Get Public and AccountID from addresses.
@@ -52,13 +52,13 @@ pub fn get_account_id_from_address<TPublic: Public>(address: &[u8; 32]) -> Accou
 	AccountPublic::from(get_from_address::<TPublic>(address)).into_account()
 }
 
-pub fn authority_keys_from_address(addr_sr25519: &[u8; 32], addr_ed25519: &[u8; 32]) -> (AuraId, GrandpaId) {
+/*pub fn authority_keys_from_address(addr_sr25519: &[u8; 32], addr_ed25519: &[u8; 32]) -> (AuraId, GrandpaId) {
 
 	(
 		get_from_address::<AuraId>(addr_sr25519),
 		get_from_address::<GrandpaId>(addr_ed25519),
 	)
-}
+}*/
 
 
 
@@ -73,13 +73,13 @@ fn list_to_accounts(list: &Vec<([u8; 32],[u8; 32])>) -> Vec<AccountId> {
 	ans
 }
 
-fn list_to_authorities(list: &Vec<([u8; 32],[u8; 32])>) -> Vec<(AuraId, GrandpaId)> {
+/*fn list_to_authorities(list: &Vec<([u8; 32],[u8; 32])>) -> Vec<(AuraId, GrandpaId)> {
 	let mut ans: Vec<(AuraId, GrandpaId)> = vec![] ;
 	for (addr_sr25519,addr_ed25519) in list {
 		ans.push(authority_keys_from_address(&addr_sr25519, &addr_ed25519)) ;
 	}
 	ans
-}
+}*/
 
 
 
@@ -98,23 +98,23 @@ fn initial_nodes_dev() -> Vec<([u8; 32],[u8; 32])> {
 			hex!("88dc3417d5058ec4b4503e0c12ea1a0a89be200fe98922423d4334014fa6b0ee")
 		),
 
-		// Bob
+		/* Bob
 		(
 			hex!("8eaf04151687736326c9fea17e25fc5287613693c912909cb226aa4794f26a48"),
 			hex!("d17c2d7823ebf260fd138f2d7e27d114c0145d968b5ff5006125f2414fadae69")
 		),
 
-		// Charlie
+		 Charlie
 		(
 			hex!("90b5ab205c6974c9ea841be688864633dc9ca8a357843eeacf2314649965fe22"),
 			hex!("439660b36c6c03afafca027b910b4fecf99801834c62a5e6006f27d978de234f")
 		),
 
-		// Dave
+		 Dave
 		(
 			hex!("306721211d5404bd9da88e0204360a1a9ab8b87c66c1bc2fcdd37f3c2222cc20"),
 			hex!("5e639b43e0052c47447dac87d6fd2b6ec50bdd4d0f614e4299c665249bbd09d9")
-		),
+		),*/
 	]
 }
 
@@ -157,6 +157,7 @@ fn initial_nodes_main() -> Vec<([u8; 32],[u8; 32])> {
 
 pub fn dev_config() -> Result<ChainSpec, String> {
 	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
+	let nodes = initial_nodes_dev() ;
 
 	Ok(ChainSpec::from_genesis(
 		"Wika Development Environment",
@@ -164,17 +165,9 @@ pub fn dev_config() -> Result<ChainSpec, String> {
 		ChainType::Development,
 		move || wika_genesis(
 			wasm_binary,
-			vec![
-				authority_keys_from_seed("Alice"),
-				authority_keys_from_seed("Bob")
-			],
 			get_account_id_from_seed::<sr25519::Public>("Alice"),
-			vec![
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				get_account_id_from_seed::<sr25519::Public>("Bob"),
-				get_account_id_from_seed::<sr25519::Public>("Charlie"),
-				get_account_id_from_seed::<sr25519::Public>("Dave")
-			],
+			&nodes,
+			list_to_accounts(&nodes),
 			true,
 		),
 		// Bootnodes
@@ -192,7 +185,7 @@ pub fn dev_config() -> Result<ChainSpec, String> {
 
 pub fn test_config() -> Result<ChainSpec, String> {
 	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
-	let test_nodes = initial_nodes_test() ;
+	let nodes = initial_nodes_test() ;
 
 	Ok(ChainSpec::from_genesis(
 		// Name
@@ -202,9 +195,9 @@ pub fn test_config() -> Result<ChainSpec, String> {
 		ChainType::Live,
 		move || wika_genesis(
 			wasm_binary,
-			list_to_authorities(&test_nodes),
 			get_account_id_from_address::<sr25519::Public>(&hex!("56442d2ddbace4927f284c799c0e706ce7e0df06f395e2c1bbfb967ece5cf053")),
-			list_to_accounts(&test_nodes),
+			&nodes,
+			list_to_accounts(&nodes),
 			true,
 		),
 		// Bootnodes
@@ -222,7 +215,7 @@ pub fn test_config() -> Result<ChainSpec, String> {
 
 pub fn main_config() -> Result<ChainSpec, String> {
 	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
-	let main_nodes = initial_nodes_main() ;
+	let nodes = initial_nodes_main() ;
 
 	Ok(ChainSpec::from_genesis(
 		"Wika MainNet",
@@ -230,10 +223,9 @@ pub fn main_config() -> Result<ChainSpec, String> {
 		ChainType::Live,
 		move || wika_genesis(
 			wasm_binary,
-			list_to_authorities(&main_nodes),
 			get_account_id_from_address::<sr25519::Public>(&hex!("a47f12db28111cfc0052494f434f4bf2cd316dcd13852d6f64e538d861c23534")),
-			// Pre-funded accounts
-			list_to_accounts(&main_nodes),
+			&nodes,
+			list_to_accounts(&nodes),
 			true,
 		),
 		// Bootnodes
@@ -252,8 +244,8 @@ pub fn main_config() -> Result<ChainSpec, String> {
 /// Configure initial storage state for FRAME modules.
 fn wika_genesis(
 	wasm_binary: &[u8],
-	initial_authorities: Vec<(AuraId, GrandpaId)>,
 	root_key: AccountId,
+	initial_authorities: &Vec<([u8; 32],[u8; 32])>,
 	endowed_accounts: Vec<AccountId>,
 	_enable_println: bool,
 ) -> GenesisConfig {
@@ -267,16 +259,16 @@ fn wika_genesis(
 			balances: endowed_accounts.iter().cloned().map(|k|(k, INITIAL_AUTHORITIES_BALANCE)).collect(),
 		}),
 		pallet_aura: Some(AuraConfig {
-			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
+			authorities: vec![],
 		}),
 		pallet_grandpa: Some(GrandpaConfig {
-			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
+			authorities: vec![],
 		}),
 		pallet_sudo: Some(SudoConfig {
 			key: root_key,
 		}),
 		pallet_authorities: Some(AuthoritiesConfig {
-			keys: initial_nodes_dev(),
+			keys: initial_authorities.clone(),
 		}),
 	}
 }
